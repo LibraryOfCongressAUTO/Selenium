bbd8e1084a3c6e1cd555a3879a444167d402d62b
Erkin <Erkin@LAPTOP-CQ4SEIPQ.fios-router.home>
2018-03-29 20:32:39 -0700

    push3

--- src/test/java/UnitTests/Author_Application.java
+++ src/test/java/UnitTests/Author_Application.java
@@ -1,24 +1,57 @@
 package UnitTests;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import Util.Base;
 
 public class Author_Application extends Base {
 
 	static String AUTHORURL = "https://locexternaldev.service-now.com/auth";
+	static String AUTHORTITLE = "CIP Author Login - CIP Author Portal";
 
-	@Test(priority = 0)
-	public void Navigate_To_Author_Portal() {
+	// @Test(priority = 0)
+	public void Verify_Author_URL() {
+
 		driver.get(AUTHORURL);
-		System.out.println(driver.getCurrentUrl());
-		System.out.println("b");
+		System.out.println("Expected URL is : " + AUTHORURL);
+		System.out.println("Actual URL is : " + driver.getCurrentUrl());
+		Assert.assertEquals(driver.getCurrentUrl(), AUTHORURL);
 	}
 
-
-	@Test(priority = 1)
-	public void Positive_Author_Login() {
-		System.out.println("d");
+	// @Test(priority = 1)
+	public void Verify_Author_Title() {
+		driver.get(AUTHORURL);
+		System.out.println("Expected Title is : " + AUTHORTITLE);
+		System.out.println("Actual Title is : " + driver.getTitle());
+		Assert.assertEquals(driver.getTitle(), AUTHORTITLE);
 	}
 
-	
+	// @Test(priority = 2)
+	public void Positive_Author_Login() throws Throwable {
+		driver.get(AUTHORURL);
+		driver.findElement(By.xpath("//INPUT[@id='username']")).sendKeys("Loc1.Tester1");
+		driver.findElement(By.xpath("//INPUT[@id='password']")).sendKeys("LocTester@123!");
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//BUTTON[@name='login'][text()=' Login ']")).click();
+		String requestLCCN = driver.findElement(By.xpath("(//*[@href='?id=request_lccn']/*)[2]")).getText();
+		Assert.assertEquals(requestLCCN, "Request Library of Congress Control Number");
+	}
+
+	@Test(priority = 3)
+	public void Negative_Author_Login() throws Throwable {
+		driver.get(AUTHORURL);
+		driver.findElement(By.xpath("//INPUT[@id='username']")).sendKeys("Test");
+		driver.findElement(By.xpath("//INPUT[@id='password']")).sendKeys("Example");
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//BUTTON[@name='login'][text()=' Login ']")).click();
+		String ErrorMessage = driver.findElement(By.xpath("//*[@class='alert alert-danger ng-binding ng-scope']"))
+				.getText();
+		if (ErrorMessage.contains("kjhgfd") == true) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
+
+		}
+	}
 }
--- src/test/java/UnitTests/Publisher_Application.java
+++ src/test/java/UnitTests/Publisher_Application.java
@@ -1,26 +1,31 @@
 package UnitTests;
 
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import Util.Base;
 
 public class Publisher_Application extends Base {
 
 	static String PUBLISHERURL = "https://locexternaldev.service-now.com/pub";
-	
-	
+	static String PUBLISHERTITLE = "CIP Publisher Login - CIP Publisher Portal";
+		
 	
 	@Test(priority = 0)
-	public void Navigate_To_Publisher_Portal() {
+	public void Verify_Publisher_URL() {
+		
 		driver.get(PUBLISHERURL);
-		System.out.println(driver.getCurrentUrl());
-		System.out.println("b");
+		System.out.println("Expected URL is : " + PUBLISHERURL);
+		System.out.println("Actual URL is : " + driver.getCurrentUrl());
+		Assert.assertEquals(driver.getCurrentUrl(), PUBLISHERURL);
 	}
 
-
 	@Test(priority = 1)
-	public void Positive_Publisher_Login() {
-		System.out.println("d");
+	public void Verify_Publisher_Title() {
+		driver.get(PUBLISHERURL);
+		System.out.println("Expected Title is : " + PUBLISHERTITLE);
+		System.out.println("Actual Title is : " + driver.getTitle());
+		Assert.assertEquals(driver.getTitle(), PUBLISHERTITLE);
 	}
 
 	
